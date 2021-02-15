e092179196a725e219b71ff31681cf6c12c50a64
Omar Mejenes <ok.mejenes@innovation.es>
2021-02-14 19:36:34 -0500

    Nuevos componentes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../styles/components/app.scss';
+import '../styles/components/App.scss';
 
 function App() {
     return (
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/components/Footer.scss';
+
+const Footer = () => {
+    return (
+        <div className='Footer'>
+            <p className='Footer-title'>StoreApp</p>
+            <p className='Footer-copy'>Omar Mejenes did it!</p>
+        </div>
+    );
+};
+
+export default Footer;
